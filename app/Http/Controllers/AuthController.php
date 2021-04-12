678fcc3902442f7f25e83e2f560191b85001859c
Md.Rafiqul <rafiqul.pust.cse@gmail.com>
2021-04-12 10:52:53 +0600

    update register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests\RegisterRequest;
 use Auth;
 use DB;
 use App\User;
@@ -14,7 +13,8 @@ class AuthController extends Controller
     //check login
     public function login(Request $request){
         try{
-              if(Auth::attempt($request->only('email','password'))){
+            
+            if(Auth::attempt($request->only('email','password'))){
                 $user = Auth::user();
                 $token = $user->createToken('app')->accessToken;
                 return response([
@@ -22,41 +22,16 @@ class AuthController extends Controller
                     'token' => $token,
                     'user' => $user
                 ], 400);
-            }
-        }catch(Exception $e) {
+        }
+    }catch(Exception $e) {
             //throw $th;
             return response([
-                'message' => $e->getMessage()
+                'message' => $e->getMessage
             ], 400);
         }
 
         return response([
             'message' => "Invalid Email or Password"
         ], 401);
-    }
-
-
-    //check register
-    public function register(RegisterRequest $request){
-        
-        try{
-
-            $user = User::create([
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' => Hash::make($request->password)
-            ],400);
-            $token = $user->createToken('app')->accessToken;
-
-            return response([
-                'message' => 'Successfully registered',
-                'token' => $token,
-                'user' => $user
-            ], 400);
-        }catch(Exception $e){
-            return response([
-                'message' => $e->getMessage
-            ], 401);
-        }
     }
 }
